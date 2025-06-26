49c70c758d46901bdcdaf269f836073488855578
parkeqpapa <danielmejuto96@gmail.com>
2025-06-26 20:41:09 -0300

    refactor: add nastpec comments

--- src/contracts/Swapper.sol
+++ src/contracts/Swapper.sol
@@ -5,27 +5,80 @@ import {IERC20} from '@openzeppelin/contracts/token/ERC20/IERC20.sol';
 import {IUniswapV2Router02} from 'src/interfaces/IUniswap.sol';
 import {IWETH9} from 'src/interfaces/IWETH.sol';
 
+/**
+ * @title Swapper
+ * @notice A collective token swapping contract that pools user deposits and executes batch swaps
+ * @dev This contract allows users to deposit WETH/ETH, pools the funds, and executes a single
+ *      swap from WETH to DAI when triggered by the governor. Users receive proportional shares
+ *      of the swapped tokens based on their deposits
+ * @author parkeqpapa
+ */
 contract Swapper {
-  error NotGovernor();
-  error SwapAlreadyExecuted();
-  error AmountMustBeGreaterThanZero();
-  error NoTokensToWithdraw();
-  error NotAuthorized();
-
+  /// @notice The governor address who can execute swaps and manage the contract
   address public governor;
 
+  /// @notice Total amount of WETH deposited by all users
   uint256 public totalFromTokens;
-  mapping(address => uint256) public userBalance;
 
+  /// @notice Mapping of user addresses to their deposited WETH amounts
+  mapping(address user => uint256 amount) public userBalance;
+
+  /// @notice Flag indicating whether the swap has been executed
   bool public swapExecuted;
-  IUniswapV2Router02 public uniswap = IUniswapV2Router02(0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D);
-  IWETH9 public weth = IWETH9(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2); // fromToken
-  IERC20 public dai = IERC20(0x6B175474E89094C44Da98b954EedeAC495271d0F); // toToken
 
+  /// @notice Uniswap V2 router for executing token swaps
+  IUniswapV2Router02 public uniswap = IUniswapV2Router02(0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D);
+
+  /// @notice WETH contract (source token for swaps)
+  IWETH9 public weth = IWETH9(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
+
+  /// @notice DAI contract (target token for swaps)
+  IERC20 public dai = IERC20(0x6B175474E89094C44Da98b954EedeAC495271d0F);
+
+  /// @notice Emitted when a user provides WETH to the pool
+  /// @param user The address of the user who provided tokens
+  /// @param amount The amount of WETH provided
   event Provided(address indexed user, uint256 amount);
+
+  /// @notice Emitted when the swap is executed
+  /// @param totalFromTokens The total amount of WETH swapped
+  /// @param totalToTokens The total amount of DAI received
   event Swapped(uint256 totalFromTokens, uint256 totalToTokens);
+
+  /// @notice Emitted when a user withdraws their share
+  /// @param user The address of the user who withdrew
+  /// @param amount The amount withdrawn (original deposit amount for tracking)
   event Withdrawn(address indexed user, uint256 amount);
 
+  /// @notice Thrown when caller is not the governor
+  error NotGovernor();
+
+  /// @notice Thrown when trying to deposit after swap has been executed
+  error SwapAlreadyExecuted();
+
+  /// @notice Thrown when trying to deposit zero amount
+  error AmountMustBeGreaterThanZero();
+
+  /// @notice Thrown when user has no tokens to withdraw
+  error NoTokensToWithdraw();
+
+  /// @notice Thrown when caller is not authorized to perform the operation
+  error NotAuthorized();
+
+  /**
+   * @notice Restricts function access to the governor only
+   * @dev Reverts with NotGovernor error if caller is not the governor
+   */
+  modifier onlyGovernor() {
+    if (msg.sender != governor) revert NotGovernor();
+    _;
+  }
+
+  /**
+   * @notice Constructs the Swapper contract
+   * @dev Sets the governor and approves Uniswap router to spend WETH
+   * @param _governor The address that will govern the contract and execute swaps
+   */
   constructor(
     address _governor
   ) {
@@ -33,39 +86,45 @@ contract Swapper {
     weth.approve(address(uniswap), type(uint256).max);
   }
 
-  modifier onlyGovernor() {
-    if (msg.sender != governor) revert NotGovernor();
-    _;
-  }
-
-  // Users deposit `fromToken`
+  /**
+   * @notice Allows users to deposit WETH tokens to the pool
+   * @dev Users must approve this contract to transfer their WETH before calling
+   * @param amount The amount of WETH to deposit
+   */
   function provide(
     uint256 amount
   ) external {
     if (swapExecuted) revert SwapAlreadyExecuted();
     if (amount == 0) revert AmountMustBeGreaterThanZero();
 
-    // Update user's deposit and total deposits
     userBalance[msg.sender] += amount;
     totalFromTokens += amount;
 
-    // Transfer `fromToken` from the user to the contract
     weth.transferFrom(msg.sender, address(this), amount);
 
     emit Provided(msg.sender, amount);
   }
 
+  /**
+   * @notice Allows users to deposit ETH which gets automatically wrapped to WETH
+   * @dev ETH sent with this function is automatically converted to WETH
+   */
   function provideWithEth() external payable {
     if (swapExecuted) revert SwapAlreadyExecuted();
     userBalance[msg.sender] += msg.value;
     totalFromTokens += msg.value;
 
-    // send Msg.value to WETH
     weth.deposit{value: msg.value}();
 
     emit Provided(msg.sender, msg.value);
   }
 
+  /**
+   * @notice Executes the batch swap from WETH to DAI for all pooled funds
+   * @dev Only the governor can execute swaps. Includes 5% slippage protection
+   * @param amount The amount of WETH to swap
+   * @return The amount of DAI received from the swap
+   */
   function swap(
     uint256 amount
   ) external onlyGovernor returns (uint256) {
@@ -73,7 +132,6 @@ contract Swapper {
 
     uint256 amountDAIExpected = getRealPrice(amount);
 
-    // Permitir un 5% de slippage
     uint256 amountOutMin = amountDAIExpected * 95 / 100;
 
     address[] memory path = new address[](2);
@@ -82,28 +140,21 @@ contract Swapper {
 
     uint256[] memory amounts =
       IUniswapV2Router02(uniswap).swapExactTokensForTokens(amount, amountOutMin, path, address(this), block.timestamp);
+
+    emit Swapped(amount, amounts[1]);
     return amounts[1];
   }
 
-  function getRealPrice(
-    uint256 amountWETH
-  ) public view returns (uint256) {
-    address[] memory path = new address[](2);
-    path[0] = address(weth);
-    path[1] = address(dai);
-
-    uint256[] memory amounts = IUniswapV2Router02(uniswap).getAmountsOut(amountWETH, path);
-    return amounts[1]; // Cantidad de DAI que recibirás
-  }
-
-  // Users withdraw their share of `toToken` (or `fromToken` if swap hasn't occurred)
+  /**
+   * @notice Allows users to withdraw their proportional share of tokens
+   * @dev If swap was executed, users receive DAI proportional to their WETH deposit
+   *      If swap wasn't executed, users receive their original WETH deposit back
+   */
   function withdraw() external {
     uint256 userDeposit = userBalance[msg.sender];
     if (userDeposit == 0) revert NoTokensToWithdraw();
 
-    // Reset user's deposit
     if (swapExecuted) {
-      // Withdraw `toToken`
       uint256 totalWETHDeposited = totalFromTokens;
       uint256 userShare = (userDeposit * dai.balanceOf(address(this))) / totalWETHDeposited;
 
@@ -112,7 +163,6 @@ contract Swapper {
 
       dai.transfer(msg.sender, userShare);
     } else {
-      // Withdraw `fromToken`
       totalFromTokens -= userDeposit;
       userBalance[msg.sender] = 0;
 
@@ -122,12 +172,38 @@ contract Swapper {
     emit Withdrawn(msg.sender, userDeposit);
   }
 
+  /**
+   * @notice Resets the swap execution flag to allow new deposits
+   * @dev Only the governor can reset the swap status for a new round
+   */
   function setSwapExecuted() external onlyGovernor {
     if (msg.sender != governor) revert NotAuthorized();
     swapExecuted = false;
   }
 
-  function getGovernance() external view returns (address) {
+  /**
+   * @notice Gets the expected DAI amount for a given WETH amount
+   * @dev Queries Uniswap for current exchange rates without executing a swap
+   * @param amountWETH The amount of WETH to check the price for
+   * @return The expected amount of DAI that would be received
+   */
+  function getRealPrice(
+    uint256 amountWETH
+  ) public view returns (uint256) {
+    address[] memory path = new address[](2);
+    path[0] = address(weth);
+    path[1] = address(dai);
+
+    uint256[] memory amounts = IUniswapV2Router02(uniswap).getAmountsOut(amountWETH, path);
+    return amounts[1];
+  }
+
+  /**
+   * @notice Returns the current governor address
+   * @dev View function to check who the current governor is
+   * @return The address of the current governor
+   */
+  function getGovernance() public view returns (address) {
     return governor;
   }
 }
--- test/integration/IntegrationBase.sol
+++ test/integration/IntegrationBase.sol
@@ -1,26 +1,25 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.23;
+// // SPDX-License-Identifier: MIT
+// pragma solidity 0.8.23;
 
-import {Greeter, IGreeter} from 'contracts/Greeter.sol';
-import {Test} from 'forge-std/Test.sol';
-import {IERC20} from 'forge-std/interfaces/IERC20.sol';
-import {IWETH9} from 'src/interfaces/IWETH.sol';
+// import {Test} from 'forge-std/Test.sol';
+// import {IERC20} from 'forge-std/interfaces/IERC20.sol';
+// import {IWETH9} from 'src/interfaces/IWETH.sol';
 
-contract IntegrationBase is Test {
-  uint256 internal constant _FORK_BLOCK = 18_920_905;
+// contract IntegrationBase is Test {
+//   uint256 internal constant _FORK_BLOCK = 18_920_905;
 
-  string internal _initialGreeting = 'hola';
-  address internal _user = makeAddr('user');
-  address internal _owner = makeAddr('owner');
-  address internal _daiWhale = 0x42f8CA49E88A8fd8F0bfA2C739e648468b8f9dec;
-  IERC20 internal _dai = IERC20(0x6B175474E89094C44Da98b954EedeAC495271d0F);
-  IWETH9 public weth;
+//   string internal _initialGreeting = 'hola';
+//   address internal _user = makeAddr('user');
+//   address internal _owner = makeAddr('owner');
+//   address internal _daiWhale = 0x42f8CA49E88A8fd8F0bfA2C739e648468b8f9dec;
+//   IERC20 internal _dai = IERC20(0x6B175474E89094C44Da98b954EedeAC495271d0F);
+//   IWETH9 public weth;
 
-  IGreeter internal _greeter;
+//   IGreeter internal _greeter;
 
-  function setUp() public {
-    vm.createSelectFork(vm.rpcUrl('mainnet'), _FORK_BLOCK);
-    vm.prank(_owner);
-    weth = IWETH9(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
-  }
-}
+//   function setUp() public {
+//     vm.createSelectFork(vm.rpcUrl('mainnet'), _FORK_BLOCK);
+//     vm.prank(_owner);
+//     weth = IWETH9(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
+//   }
+// }
